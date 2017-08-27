7007bb3e6aacb6e7884ed72213f7671ad5d9114a
Luke Smith <lsmith@zenoscave.com>
2017-08-27 14:28:22 -0600

    vga/tty prints on kernel

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,5 +1,13 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+
+#include <io/tty.h>
+
 void kernel_early(void *mboot, unsigned int magic, unsigned int *ebp)
 {
+  initialize_terminal();
+  terminal_writestring("[TTY]   ... \x1b[32mDONE\n\x1b[00m");
 }
 
 void main()
